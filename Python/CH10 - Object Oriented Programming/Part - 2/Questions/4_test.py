4e7b2e583e38aca2d6e5170bba29bf3f387393ea
PranavAggarwal2002 <pranavwal@gmail.com>
2026-05-28 01:55:59 +0530

    Python6

--- Python/CH10 - Object Oriented Programming/Part - 2/Questions/4_test.py
+++ Python/CH10 - Object Oriented Programming/Part - 2/Questions/4_test.py
@@ -3,16 +3,15 @@ class Employee:
     increment = 20
 
     @property
-    def SalryAfterIncrement(self):
+    def SalaryAfterIncrement(self):
         return (self.salary + self.salary * (self.increment/100))
-    
-    @SalryAfterIncrement.setter
-    def SalryAfterIncrement(self, salary):
+
+    @SalaryAfterIncrement.setter
+    def SalaryAfterIncrement(self, salary):
         self.increment = ((salary/self.salary)-1)*100
 
-
 e = Employee()
-# print(e.SalryAfterIncrement)
+# print(e.SalaryAfterIncrement)
 
-e.SalryAfterIncrement = 280.8
+e.SalaryAfterIncrement = 280.8
 print(e.increment)
